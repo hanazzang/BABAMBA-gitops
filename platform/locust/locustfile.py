f59f409237d3141a3b9f39cab7c1ab55746fc50b
LargeSsun <hyunho31313@gmail.com>
2026-01-20 16:02:20 +0900

    feat: replace locust and k6 configurations with new config maps and remove obsolete files

--- platform/locust/locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class PhotoServiceUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
-    def access_photo(self):
-        self.client.get("/photo")
